900f8d30e902d1da463f3a157778cc069a1362ab
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-06-06 18:43:16 -0300

    atualizando next/image

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 import { SignInButton } from '../SignInButton';
+import Image from 'next/image';
 
 export function Header(){
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-              <img src="/images/logo.svg" alt="ig.news" />
+              <Image height={120} width={120} src="/images/logo.svg" alt="ig.news" />
                <nav>
                <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
